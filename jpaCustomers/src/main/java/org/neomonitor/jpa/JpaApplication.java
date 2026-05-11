4fbe9dd293c84341ebc3a8822db4d20dd54e5d69
Carlitic <carloscastanosblanco@gmail.com>
2026-05-11 17:24:54 +0200

    Ultimos retoques

--- jpaCustomers/src/main/java/org/neomonitor/jpa/JpaApplication.java
+++ jpaCustomers/src/main/java/org/neomonitor/jpa/JpaApplication.java
@@ -19,16 +19,12 @@ public class JpaApplication extends Application {
 
 	@Override
 	public void init() throws Exception {
-		// Arrancamos el motor de Spring Boot antes de mostrar la ventana
 		springContext = SpringApplication.run(JpaApplication.class);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// Importamos y cargamos el archivo de diseño FXML
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/main-view.fxml"));
-
-		// Le decimos a JavaFX que Spring se encargue de crear los controladores
 		fxmlLoader.setControllerFactory(springContext::getBean);
 
 		Parent root = fxmlLoader.load();
